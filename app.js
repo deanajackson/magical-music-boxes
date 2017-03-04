cda8da3cd1ebc215c0abf4132672b5cd77483d58
deanajackson <demaisja@gmail.com>
2017-03-04 13:47:41 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-var url = 'https://bcw.blob.core.windows.net/public/img/sounds/scale-00'
+var url = 'https://bcw.blob.core.windows.net/public/img/sounds/scale-0'
 
 var boombox = document.getElementById('boombox')
 
 function playTune(num){
   
   var tuneUrl = url + num + '.mp3'
+  
+  if(num < 10){
+     tuneUrl += 0
+    }
+    
+  tuneUrl += num + '.mp3'
   
   console.log(tuneUrl)
   
