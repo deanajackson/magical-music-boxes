372e1b8ee18452e2994457dacfdd678d1f90b855
deanajackson <demaisja@gmail.com>
2017-03-04 13:31:45 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+var url = 'https://bcw.blob.core.windows.net/public/img/sounds/scale-00'
+
+var boombox = document.getElementById('boombox')
+
+function playTune(num){
+  
+  var tuneUrl = url + num + '.mp3'
+  
+  console.log(tuneUrl)
+  
+  boombox.src = tuneUrl 
+  boombox.play()
+}
